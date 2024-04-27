3e72154f0cfe21457c72e718f9e50c04ae5a5aa9
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-04-27 18:08:56 +0500

    Update InspireApis.js

--- src/redux/InspireApis.js
+++ src/redux/InspireApis.js
@@ -1,26 +1,38 @@
 import Config from "../constants/Index";
 
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const inspireApis = createApi({
-  reducerPath: 'inspireApis',
-  baseQuery: fetchBaseQuery({ baseUrl: Config.serverApiUrl, }),
+  reducerPath: "inspireApis",
+  baseQuery: fetchBaseQuery({ baseUrl: Config.serverApiUrl }),
   endpoints: (builder) => ({
-  
-  getProducts: builder.query({
-    query: () => "products?populate=*", 
-  }),
-  getSingleProduct: builder.query({
-    query: (id) => `products/${id}`,
-  }),
-  addProduct: builder.mutation({
-    query: (data) => ({
-      url: "products",
-      method: "POST",
-      body: data,
+    getProducts: builder.query({
+      query: () => "products?populate=*",
+    }),
+    getSingleProduct: builder.query({
+      query: (id) => `products/${id}`,
+    }),
+    addProduct: builder.mutation({
+      query: (data) => ({
+        url: "products",
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateProduct: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `products/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `products/${id}`,
+        method: "DELETE",
+      }),
     }),
   }),
-  }),
-})
+});
 export const {
   useGetProductsQuery,
   useGetSingleProductQuery,
